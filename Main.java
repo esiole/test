5c08272b4ef9f882bb1c5a01079037dcab850a25
esiole <31545768+esiole@users.noreply.github.com>
2019-08-01 07:58:29 +0300

    Revert "изменение"
    This reverts commit 8d5d757e15d3dd4f781fe000aaedf22041bab509.

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import gui.*;
 import logger.Logs;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Logs.writeLog("start program", Level.INFO);
