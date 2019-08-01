5f7b07dcc20ea6dd6bf95dd8f4616e0541aa46b3
esiole <31545768+esiole@users.noreply.github.com>
2019-08-01 11:02:55 +0300

    O!

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import gui.*;
 import logger.Logs;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Logs.writeLog("start program", Level.INFO);
         MainWindow window = new MainWindow();
         window.setVisible(true);
