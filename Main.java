c00a87d0393b8a70f926327f6d359eebc57f13b2
esiole <31545768+esiole@users.noreply.github.com>
2019-08-01 11:01:01 +0300

    LOL!

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import gui.*;
 import logger.Logs;
 
-import java.io.IOException;
-import java.util.logging.Level;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         Logs.writeLog("start program", Level.INFO);
