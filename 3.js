7bc96a545260b45e9b2db479b4770b46c635bca8
esiole <31545768+esiole@users.noreply.github.com>
2019-12-07 14:05:47 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -9,7 +9,7 @@ chrome.extension.onMessage.addListener(function(request, sender, response) {
     }
     return true;
 });
-d233dd31fsgags
+d233dd31fsgagsf
 function createCommitsList(path, url) {
     let label = document.getElementsByClassName("commit-tease js-details-container Details d-flex rounded-top-1");
     if (label[0] !== undefined && label.length === 1) {
